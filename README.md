d8af20b66065c386f8ad9f74567fcbede6b2d706
sasi_n <sasivarnan.nataraja@dws.com.au>
2017-04-18 01:16:57 +1000

    Version 1.1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sn-optus-challenge
+# Requirement Java 8
+# Runnable jar loaded in /target/sn-optus-challenge-0.0.1-SNAPSHOT.jar
+# Run Command : java -jar sn-optus-challenge-0.0.1-SNAPSHOT.jar
+# URL Format : http://localhost:8003/subscribe/{The original string}/{The total number of characters that are allowed in the result}
+# Example : http://localhost:8003/subscribe/123456789012345678901234567890/31
--- src/main/java/com/optus/challenge/service/Subscriber.java
+++ src/main/java/com/optus/challenge/service/Subscriber.java
@@ -41,33 +41,28 @@ public class Subscriber {
 	 * @return
 	 */
 	private String truncate(String orderDetails,int expectedLength){
-		
+		System.out.println("--INPUT--> "+orderDetails);
 		int fixedTextLength=fixedText.length();
 		int originalLength=orderDetails.length();
 		
 		if(expectedLength==0){
+			System.out.println("--OUTPUT--> "+orderDetails);
 			return orderDetails;
 		}
-		/*else if (expectedLength <= fixedTextLength){
-			return "Error : Expected length paramteter is too small";
-		}*/
 		else if(originalLength<=fixedTextLength){
+			System.out.println("--OUTPUT--> "+orderDetails);
 			return orderDetails;
 		}
 		else if(originalLength<=expectedLength){
+			System.out.println("--OUTPUT--> "+orderDetails);
 			return orderDetails;
 		}
 		else if(originalLength > expectedLength){
 			
 			if(originalLength>fixedTextLength){
-				/*if(originalLength-fixedTextLength >=4){
-					return getShortTest(orderDetails);
-				}*/
-				System.out.println("----0000------ "+orderDetails);
 				StringBuffer text= new StringBuffer(orderDetails);
 				int diff=originalLength-expectedLength;
 				int mid=Math.abs(originalLength/2);
-				System.out.println("Diff "+diff+" Mid "+mid);
 				int start=0;
 				if(diff%2 == 0){
 					start=mid - Math.abs(diff/2);
@@ -77,33 +72,17 @@ public class Subscriber {
 				}
 				int end=mid + Math.abs(diff/2);
 				text.delete(start, end);
-				System.out.println("Start "+start+" End "+end);
-				System.out.println("----1111------ "+text.toString());
 				int mid1=Math.abs(text.length()/2);
-				System.out.println("Mid1 "+mid1);
 				int start1=mid1 - Math.abs(fixedTextLength/2);
 				int end1=mid1 + Math.abs(fixedTextLength/2);
 				text.replace(start1, end1, fixedText);
-				System.out.println("Start1 "+start+" End1 "+end);
-				System.out.println("----2222------ "+text.toString());
+				System.out.println("--OUTPUT--> "+text.toString());
 				return text.toString();
 			}
 			
 		}
 		return null;
 	}
-	
-	/**
-	 * 
-	 * @param orderDetails
-	 * @return
-	 */
-	private String getShortTest(String orderDetails){
-		StringBuffer text= new StringBuffer(orderDetails);
-		text.delete(2, text.length()-2);
-		text.insert(2,fixedText);
-		return text.toString();
-	}
-	
+		
 
 }
